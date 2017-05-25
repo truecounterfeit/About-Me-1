89714c872b660be9c2d1b0bf556ea6dfbd7d269c
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-05-24 21:33:54 -0700

    Initial commit with project scaffolding

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var confirmed = confirmed('Are you have your mind blowen!?');
+
+if (confirmed === true){
+  console.log('The User is ready!');
+} else {
+  console.log('User is too scared but we are going to continue anyway.')
+};
