71deacbea43cd870273d60c353958c4c4f97f530
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-05-25 19:33:40 -0700

    Adding change for Lab tonight

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-'use strict';
+'use strict'
 
-var confirmed = confirmed('Are you have your mind blowen!?');
+var confirmed = confirm('Are you have your mind blowen!?');
 
 if (confirmed === true){
   console.log('The User is ready!');
 } else {
   console.log('User is too scared but we are going to continue anyway.')
 };
+ var confirmed =confirm('Are you looking for a new kick ass develpoer to be on your team?');
+
+ if (confirmed === false){
+   console.log('You are missing out on something amazing!')
+ }else {
+   console.log ('Then you have come to the right place.')
+ };
