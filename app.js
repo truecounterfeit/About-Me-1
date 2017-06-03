d6028b359fc037be8ff748794b02aead8ae115f7
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-03 13:32:58 -0700

    Adding a brach new questions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//question 1
 var confirmed = confirm('Are you have your mind blowen!?');
 
 if (confirmed === true){
@@ -7,6 +7,7 @@ if (confirmed === true){
 } else {
   console.log('User is too scared but we are going to continue anyway.')
 };
+//question 2
  var confirmed =confirm('Are you looking for a new kick ass develpoer to be on your team?');
 
  if (confirmed === false){
@@ -14,3 +15,28 @@ if (confirmed === true){
  }else {
    console.log ('Then you have come to the right place.')
  };
+ //question 3
+var confirmed = confirm('Want to play a game?');
+
+if (confirmed === true){
+  console.log('User wants to play.')
+} else {
+  console.log('User refuses to leave.')
+};
+//question 4
+var confirmed = confirm('Are yuo sure you raed this correctly?');
+
+if (confirmed === true){
+  console.log('User is lying')
+} else {
+  console.log('User passed')
+};
+//question 5
+var confirmed = confirm('Do you know what Taco Cat spelled backwards is Taco Cat?');
+
+if (confirmed === true){
+  console.log('User lies.')
+} else {
+  console.log('User is confused')
+};
+//question 6
