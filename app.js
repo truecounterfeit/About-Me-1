fca564f885704ecc3f7b0eeb00840f151dde0fc8
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro.local>
2017-06-03 14:30:15 -0700

    fixed quetions added function

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
-'use strict'
+'use strict';
+
+var function = if (question().toUpperCase === 'YES' || 'Y') {
+  prompt('Correct!');
+} else {
+  prompt('Wrong!');
+}
+questions('Can pigs fly?');
+
 //question 1
 var confirmed = confirm('Are you have your mind blowen!?');
-
 if (confirmed === true){
   console.log('The User is ready!');
 } else {
-  console.log('User is too scared but we are going to continue anyway.')
+  console.log('User is too scared but we are going to continue anyway.');
 };
 //question 2
- var confirmed =confirm('Are you looking for a new kick ass develpoer to be on your team?');
+var confirmed = confirm('Are you looking for a new kick ass develpoer to be on your team?');
 
- if (confirmed === false){
-   console.log('You are missing out on something amazing!')
- }else {
-   console.log ('Then you have come to the right place.')
- };
+if (confirmed === false){
+  console.log('You are missing out on something amazing!');
+} else {
+  console.log ('Then you have come to the right place.');
+};
  //question 3
 var confirmed = confirm('Want to play a game?');
 
 if (confirmed === true){
-  console.log('User wants to play.')
+  console.log('User wants to play.');
 } else {
-  console.log('User refuses to leave.')
+  console.log('User refuses to leave.');
 };
 //question 4
 var confirmed = confirm('Are yuo sure you raed this correctly?');
 
 if (confirmed === true){
-  console.log('User is lying')
+  console.log('User is lying');
 } else {
-  console.log('User passed')
+  console.log('User passed');
 };
 //question 5
 var confirmed = confirm('Do you know what Taco Cat spelled backwards is Taco Cat?');
 
 if (confirmed === true){
-  console.log('User lies.')
+  console.log('User lies.');
 } else {
-  console.log('User is confused')
+  console.log('User is confused');
 };
 //question 6
